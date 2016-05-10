2294b24ece119f7c76685890b02e23efa846e474
Eric Duong <eduong117@gmail.com>
2016-05-09 22:53:43 -0700

    added code for weight calculations

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var main = function() {
-$
+$(.button).click(function() {
+	var totalWeight = $(".form-control").val();
+	totalWeight = (totalWeight-45)/2;
+	var fortyFive = Math.floor(totalWeight/45); 
+	$('<li>').text(fortyFive).prependTo(".weights");
+	var thirtyFive = fortyFive % 45;
 
+	});
 }
 
 $(document).ready(main);
