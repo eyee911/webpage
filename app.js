4301e3e12fb2e55b82528ff44c498e000bded37d
Eric Duong <eduong117@gmail.com>
2016-05-10 20:31:42 -0700

    added input weight functionality

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var main = function() {
-$(.button).click(function() {
+$('.button').click(function() {
 	var totalWeight = $(".form-control").val();
 	totalWeight = (totalWeight-45)/2;
 	var fortyFive = Math.floor(totalWeight/45); 
-	$('<li>').text(fortyFive).prependTo(".weights");
-	var thirtyFive = fortyFive % 45;
+	var thirtyFive = Math.floor((totalWeight - fortyFive * 45)/35);
+	var twentyFive = Math.floor((totalWeight - (fortyFive * 45) - (thirtyFive * 35))/25);
+	var ten = Math.floor((totalWeight - (fortyFive * 45) - (thirtyFive * 35) - (twentyFive * 25))/10);
+	var five = Math.floor((totalWeight -  (fortyFive * 45) - (thirtyFive * 35) - (twentyFive * 25) - (ten * 10))/5);
+	var twoHalf = Math.floor((totalWeight - (fortyFive * 45) - (thirtyFive * 35) - (twentyFive * 25) - (ten * 10) - (five * 5))/2.5);
+	$('<li>').text(fortyFive + " 45 plates").prependTo(".weights");
+	$('<li>').text(thirtyFive + " 35 plates").prependTo(".weights");
+	$('<li>').text(twentyFive + " 25 plates").prependTo(".weights");	
+	$('<li>').text(ten + " 10 plates").prependTo(".weights");
+	$('<li>').text(five + " 5 plates").prependTo(".weights");
+	$('<li>').text(twoHalf + " 2.5 plates").prependTo(".weights");
 
-	});
+	
+});
 }
 
 $(document).ready(main);
