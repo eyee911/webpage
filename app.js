e8a45d198bb43c1bbdf4c4cdc35864a052c11feb
Eric Duong <eduong117@gmail.com>
2016-05-11 22:42:30 -0700

    added input verification

--- app.js
+++ app.js
@@ -8,15 +8,28 @@ $('.button').click(function() {
 	var ten = Math.floor((totalWeight - (fortyFive * 45) - (thirtyFive * 35) - (twentyFive * 25))/10);
 	var five = Math.floor((totalWeight -  (fortyFive * 45) - (thirtyFive * 35) - (twentyFive * 25) - (ten * 10))/5);
 	var twoHalf = Math.floor((totalWeight - (fortyFive * 45) - (thirtyFive * 35) - (twentyFive * 25) - (ten * 10) - (five * 5))/2.5);
-	$('<li>').text(fortyFive + " 45 plates").prependTo(".weights");
-	$('<li>').text(thirtyFive + " 35 plates").prependTo(".weights");
-	$('<li>').text(twentyFive + " 25 plates").prependTo(".weights");	
-	$('<li>').text(ten + " 10 plates").prependTo(".weights");
-	$('<li>').text(five + " 5 plates").prependTo(".weights");
-	$('<li>').text(twoHalf + " 2.5 plates").prependTo(".weights");
 
+	if(fortyFive>0) {
+		$('<li>').text(fortyFive + " 45 plates").prependTo(".weights");
+	}
+	if(thirtyFive>0) {
+		$('<li>').text(thirtyFive + " 35 plates").prependTo(".weights");
+	}
+	if(twentyFive>0) {
+		$('<li>').text(twentyFive + " 25 plates").prependTo(".weights");
+	}
+	if(ten>0) {
+	 $('<li>').text(ten + " 10 plates").prependTo(".weights");
+	}
+	if(five>0){
+		$('<li>').text(five + " 5 plates").prependTo(".weights");
+	}
+	if(twoHalf>0) {
+		$('<li>').text(twoHalf + " 2.5 plates").prependTo(".weights");
+	}
 	
 });
+$('.button').addClass('disabled');
 }
 
 $(document).ready(main);
